643aad443b3d143f1448e4eb88fcf5f631b80454
Michael Enger <michaelenger@live.com>
2018-07-17 21:27:10 +0200

    Conform to pylint

--- fhack.py
+++ fhack.py
@@ -1,90 +1,94 @@
+"""Hacker for the terminals in Fallout"""
+
 import random
 import sys
 
-class FalloutHacker:
-  """Hacker for the terminals in Fallout"""
-
-  def __init__(self, words):
-    """Creates the hacker with a specified list of words."""
-    self.all_words = list(map(lambda x: x.upper(), filter(lambda x: x, words)))
-    self.current_word = None
-    self.reset()
-
-  def compare(self, word, other):
+def compare_words(word, other):
     """Compare the likeness of two words"""
     likeness = 0
-    for i in range(0, len(word)):
-      if word[i] == other[i]:
-        likeness += 1
+    for i, letter in enumerate(word):
+        if letter == other[i]:
+            likeness += 1
     return likeness
 
-  def eliminate_word(self, word, likeness):
-    """Eliminate a word from the list of possible words"""
-    self.possible_words.remove(word)
-    self.attempted_words.append((word, likeness))
+class FalloutHacker:
+    """Hacker for the terminals in Fallout"""
 
-  def has_words(self):
-    """Checks if the hacker has possible words"""
-    return len(self.possible_words) != 0
+    def __init__(self, words):
+        """Creates the hacker with a specified list of words."""
+        self.all_words = list(map(lambda x: x.upper(), filter(lambda x: x, words)))
+        self.current_word = None
+        self.reset()
 
-  def next(self, likeness=None):
-    """Gets the next word (or None) if there are no more words"""
-    if likeness != None and self.current_word != None:
-      self.eliminate_word(self.current_word, likeness)
-    return self.suggest_word() if self.has_words() else None
+    def eliminate_word(self, word, likeness):
+        """Eliminate a word from the list of possible words"""
+        self.possible_words.remove(word)
+        self.attempted_words.append((word, likeness))
 
-  def reset(self):
-    """Resets the hack, allowing you to try again (if the input was wrong or anything)"""
-    self.possible_words = self.all_words.copy()
-    self.attempted_words = list()
+    def has_words(self):
+        """Checks if the hacker has possible words"""
+        return len(self.possible_words) != 0
 
-  def suggest_word(self):
-    """Suggest one of the words from the list of possible words"""
-    suggestions = list()
+    def next(self, likeness=None):
+        """Gets the next word (or None) if there are no more words"""
+        if likeness is not None and self.current_word is not None:
+            self.eliminate_word(self.current_word, likeness)
+        return self.suggest_word() if self.has_words() else None
 
-    if len(self.attempted_words) != 0:
-      for word in self.possible_words:
-        similarity = 0
-        for attempted in self.attempted_words:
-          likeness = self.compare(word, attempted[0])
-          if likeness > 0 and attempted[1] == 0:
-            similarity -= 1
-          elif likeness >= attempted[1]:
-            similarity += 1
-        suggestions.append((word, similarity))
+    def reset(self):
+        """Resets the hack, allowing you to try again (if the input was wrong or anything)"""
+        self.possible_words = self.all_words.copy()
+        self.attempted_words = list()
 
-    suggestion = None
-    if len(suggestions) != 0:
-      suggestions = sorted(suggestions, key=lambda x: x[1], reverse=True)
-      suggestion = suggestions[0][0]
-    else:
-      suggestion = random.choice(self.possible_words)
+    def suggest_word(self):
+        """Suggest one of the words from the list of possible words"""
+        suggestions = list()
 
-    self.current_word = suggestion
-    return suggestion
+        if self.attempted_words:
+            for word in self.possible_words:
+                similarity = 0
+                for attempted in self.attempted_words:
+                    likeness = compare_words(word, attempted[0])
+                    if likeness > 0 and attempted[1] == 0:
+                        similarity -= 1
+                    elif likeness >= attempted[1]:
+                        similarity += 1
+                suggestions.append((word, similarity))
+
+        suggestion = None
+        if suggestions:
+            suggestions = sorted(suggestions, key=lambda x: x[1], reverse=True)
+            suggestion = suggestions[0][0]
+        else:
+            suggestion = random.choice(self.possible_words)
+
+        self.current_word = suggestion
+        return suggestion
 
 
 # Running the script directly
 
 def main():
-  if len(sys.argv) > 1:
-    words = sys.argv[1:]
-  else:
-    print("Enter all the available words, separated by a spaces:")
-    words = input().split(" ")
+    """Hacker for the terminals in Fallout"""
 
-  hacker = FalloutHacker(words)
+    if len(sys.argv) > 1:
+        words = sys.argv[1:]
+    else:
+        print("Enter all the available words, separated by a spaces:")
+        words = input().split(" ")
 
-  print("Enter the likeness value for the chosen words (leave empty to quit)")
-  while hacker.has_words():
-    choice = hacker.suggest_word()
-    likeness = input(choice + ": ")
+    hacker = FalloutHacker(words)
 
-    if likeness == "":
-      break # lol we done
+    print("Enter the likeness value for the chosen words (leave empty to quit)")
+    while hacker.has_words():
+        choice = hacker.suggest_word()
+        likeness = input(choice + ": ")
 
-    likeness = int(likeness)
-    hacker.eliminate_word(choice, likeness)
+        if likeness == "":
+            break # lol we done
+
+        likeness = int(likeness)
+        hacker.eliminate_word(choice, likeness)
 
 if __name__ == "__main__":
-  main()
+    main()
--- test.py
+++ test.py
@@ -1,77 +1,76 @@
 import unittest
-from fhack import FalloutHacker
+from fhack import FalloutHacker, compare_words
 
 class TestFalloutHacker(unittest.TestCase):
-  def compare(self, word, other):
-    likeness = 0
-    for i in range(0, len(word)):
-      if word[i] == other[i]:
-        likeness += 1
-    return likeness
+    def compare(self, word, other):
+        likeness = 0
+        for i, letter in enumerate(word):
+            if letter == other[i]:
+                likeness += 1
+        return likeness
 
-  def test_compare(self):
-    hack = FalloutHacker(["TEST"])
-    self.assertEqual(hack.compare("TEST", "TEST"), 4)
-    self.assertEqual(hack.compare("TEST", "BEST"), 3)
-    self.assertEqual(hack.compare("TEST", "NONE"), 0)
+    def test_compare_words(self):
+        self.assertEqual(compare_words("TEST", "TEST"), 4)
+        self.assertEqual(compare_words("TEST", "BEST"), 3)
+        self.assertEqual(compare_words("TEST", "NONE"), 0)
 
-  def test_eliminate_word(self):
-    hack = FalloutHacker(["TEST"])
-    self.assertTrue(hack.has_words())
-    hack.eliminate_word("TEST", 2)
-    self.assertFalse(hack.has_words())
+    def test_eliminate_word(self):
+        hack = FalloutHacker(["TEST"])
+        self.assertTrue(hack.has_words())
+        hack.eliminate_word("TEST", 2)
+        self.assertFalse(hack.has_words())
 
-  def test_eliminate_word_two_words(self):
-    hack = FalloutHacker(["cat", "hat"])
-    hack.eliminate_word("CAT", 2)
-    word = hack.suggest_word()
-    self.assertEqual(word, "HAT")
+    def test_eliminate_word_two_words(self):
+        hack = FalloutHacker(["cat", "hat"])
+        hack.eliminate_word("CAT", 2)
+        word = hack.suggest_word()
+        self.assertEqual(word, "HAT")
 
-  def test_next(self):
-    hack = FalloutHacker(["cat", "hat", "fat"])
-    for i in range(3):
-      word = hack.next()
-      self.assertIn(word, ["CAT", "HAT", "FAT"])
-      hack.next(2)
-    word = hack.next()
-    self.assertIsNone(word)
+    def test_next(self):
+        hack = FalloutHacker(["cat", "hat", "fat"])
+        for i in range(3):
+            word = hack.next()
+            self.assertIn(word, ["CAT", "HAT", "FAT"])
+            hack.next(2)
+        word = hack.next()
+        self.assertIsNone(word)
 
-  def test_has_words(self):
-    hack = FalloutHacker(["TEST"])
-    self.assertTrue(hack.has_words())
-    hack.eliminate_word("TEST", 2)
-    self.assertFalse(hack.has_words())
+    def test_has_words(self):
+        hack = FalloutHacker(["TEST"])
+        self.assertTrue(hack.has_words())
+        hack.eliminate_word("TEST", 2)
+        self.assertFalse(hack.has_words())
 
-  def test_reset(self):
-    hack = FalloutHacker(["TEST"])
-    self.assertTrue(hack.has_words())
-    hack.eliminate_word("TEST", 2)
-    self.assertFalse(hack.has_words())
-    hack.reset()
-    self.assertTrue(hack.has_words())
+    def test_reset(self):
+        hack = FalloutHacker(["TEST"])
+        self.assertTrue(hack.has_words())
+        hack.eliminate_word("TEST", 2)
+        self.assertFalse(hack.has_words())
+        hack.reset()
+        self.assertTrue(hack.has_words())
 
-  def test_suggest_word_single_word(self):
-    hack = FalloutHacker(["Test"])
-    word = hack.suggest_word()
-    self.assertEqual(word, "TEST")
+    def test_suggest_word_single_word(self):
+        hack = FalloutHacker(["Test"])
+        word = hack.suggest_word()
+        self.assertEqual(word, "TEST")
 
-  def test_suggest_word_multiple_words(self):
-    hack = FalloutHacker(["TEST", "again"])
-    word = hack.suggest_word()
-    self.assertIn(word, ["TEST", "AGAIN"])
+    def test_suggest_word_multiple_words(self):
+        hack = FalloutHacker(["TEST", "again"])
+        word = hack.suggest_word()
+        self.assertIn(word, ["TEST", "AGAIN"])
 
-  def test_hack(self):
-    hack = FalloutHacker([
-        "dancing", "talking", "walking", "command", "pattern", "history",
-        "milling", "torture", "warrior", "sealant", "tyranny", "cousins"
-    ])
-    target = "WARRIOR"
-    word = ""
-    while word != target and hack.has_words():
-      word = hack.suggest_word()
-      if word != target:
-        hack.eliminate_word(word, self.compare(word, target))
-    self.assertEqual(word, target)
+    def test_hack(self):
+        hack = FalloutHacker([
+            "dancing", "talking", "walking", "command", "pattern", "history",
+            "milling", "torture", "warrior", "sealant", "tyranny", "cousins"
+        ])
+        target = "WARRIOR"
+        word = ""
+        while word != target and hack.has_words():
+            word = hack.suggest_word()
+            if word != target:
+                hack.eliminate_word(word, self.compare(word, target))
+        self.assertEqual(word, target)
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
